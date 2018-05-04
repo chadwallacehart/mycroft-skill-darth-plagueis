2f9887671c29a70055f72e39d59b287dc7a1a3e5
Chad Hart <chadwallacehart>
2018-05-04 17:04:21 -0400

    clean-up for github

---
+++ README.md
@@ -0,0 +1,19 @@
+## Mycroft Tragedy of Darth Plagueis skill
+Have you ever heard the Tragedy of Darth Plaguies the Wise? I thought not, its not a story the Jedi would tell.
+
+## Description 
+Recreate the Star Wars Revenge of the Sith scene where Palpatine explains the Tragedy of Darth Plaguies to Anakin.
+You must start the skill by asking about "Darth Plaguies". After that you can navigate via any of Anakin's dialog 
+in that scene and the app will respond as Palpatine.
+
+## Usage 
+See this [video](https://www.youtube.com/embed/WyUibKkECNc) for the full dialog.
+
+### Sample input phrases
+* "Who was Darth Plagueis?"
+* "He could actuall save people from death?"
+* "What happened to him?"
+* "Can this power be learned?"
+
+## More info 
+I wrote a blog on this here: https://chadwallacehart.com/making-a-mycroft-skill
--- __init__.py
+++ __init__.py
@@ -12,12 +12,18 @@ from mycroft.skills.core import MycroftSkill, intent_handler
 from mycroft.skills.context import adds_context, removes_context
 from mycroft.skills.core import FallbackSkill
 from mycroft.util.log import LOG
+from mycroft.util import play_wav as play
 from time import sleep
+import os
+import sys
 
+skill_path = "/opt/mycroft/skills/skill-darth-plagueis/"
+sys.path.append(skill_path)
+import led # for led control on the aiy voice kit
 
-# Each skill is contained within its own class, which inherits base methods
-# from the MycroftSkill class.  You extend this class as shown below.
-
+# This was my first test based on the default skill template
+# See the 2nd class below for more sophisticated dialog control
+# ToDo: delete
 class PlagueisSkillBasic(MycroftSkill):
 
     # The constructor of the skill, which calls MycroftSkill's constructor
@@ -42,8 +48,6 @@ class PlagueisSkillBasic(MycroftSkill):
         # In this case, respond by simply speaking a canned response.
         # Mycroft will randomly speak one of the lines from the file
         #    dialogs/en-us/hello.world.dialog
-        # self.speak_dialog("tragedy1", expect_response=True)
-
 
         def no_response():
             self.speak_dialog("tragedy5")
@@ -88,20 +92,30 @@ class PlagueisSkillBasic(MycroftSkill):
 
 
 class PlagueisSkillContext(MycroftSkill):
-    def __init__(self):
+    def __init__(self, mode='tts'):
         super(PlagueisSkillContext, self).__init__(name='PlagueisSkill')
         self.darkside = False
         self.started = False
-        LOG.debug("Darth Plagueis skill loaded")
+        if mode=='wav':
+            self.wav_mode = True
+        else:
+            self.wav_mode = False
+
+        LOG("PlagueisSKill").debug("Darth Plagueis skill loaded")
 
     def story(self):
-        self.speak("I thought not.")
-        self.speak("It's not a story the Jedi would tell you.")
-        self.speak("It's a Sith legend.")
-        self.speak("Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he could use the Force "
-                   "to influence the midichlorians to create life...")
-        self.speak("He had such a knowledge of the dark side that he could even keep the ones he cared about "
-                   "from dying.", expect_response=True)
+        if self.wav_mode:
+            play(os.path.join(skill_path,'audio/tragedy of darth plagueis 2.wav'))
+            led.wait(35)
+            self.speak(" ", expect_response=True)
+        else:
+            self.speak("I thought not.")
+            self.speak("It's not a story the Jedi would tell you.")
+            self.speak("It's a Sith legend.")
+            self.speak("Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he could use the Force "
+                       "to influence the midichlorians to create life...")
+            self.speak("He had such a knowledge of the dark side that he could even keep the ones he cared about "
+                       "from dying.", expect_response=True)
 
 
     @intent_handler(IntentBuilder('DarthPlagueis').require('Darth_Plagueis'))
@@ -109,59 +123,96 @@ class PlagueisSkillContext(MycroftSkill):
     def handle_darth_plagueis_intent(self, message):
         self.started = True
 
-        #choice = self.get_response("Did you ever hear the tragedy of Darth Plagueis the Wise?")
-        #self.speak("Did you ever hear the tragedy of Darth Plagueis the Wise?")
-        response = self.get_response("tragedy1")
+        # choice = self.get_response("Did you ever hear the tragedy of Darth Plagueis the Wise?")
+        if self.wav_mode:
+            play(os.path.join(skill_path,'audio/tragedy of darth plagueis 1.wav'))
+            led.wait(5)
+            self.speak(" ", expect_response=True)
+
+        else:
+            self.speak("Did you ever hear the tragedy of Darth Plagueis the Wise?")
+
+        # ToDo: find a better way to move to the next line if there is no response
+        # the blank dialog below does not work with the Google TTS, so removed
+        '''
+        response = self.get_response('blank')
 
         if response == "yes" or response == "yeah":
             self.speak("Not from a Jedi")
             self.remove_context('PlagueisContext')
         else:
             self.story()
+        '''
 
-    '''
+    # ToDo: set the 'yes' and 'no' below to only work after the initial "Did you ever hear.."
     @intent_handler(IntentBuilder("NotFamiliarIntent").require('no').require('PlagueisContext').build())
     def handle_not_familiar_intent(self, message):
         self.story()
 
-    @intent_handler(IntentBuilder("NotFamiliarIntent").require('yes').require('PlagueisContext').build())
+
+    @intent_handler(IntentBuilder("FamiliarIntent").require('yes').require('PlagueisContext').build())
     @removes_context('PlagueisContext')
-    def handle_yes_familiar_intent(self, message):
-        self.speak("Not from a Jedi")
-    '''
+    def handle_familiar_intent(self, message):
+        if self.wav_mode:
+            play(os.path.join(skill_path,'audio/tragedy of darth plagueis 5.wav'))
+            led.wait(1.2, led.LED.ON)
+        else:
+            self.speak("Not from a Jedi")
+
 
     @intent_handler(IntentBuilder('SaveFromDeathIntent').require('save_death').require('PlagueisContext').build())
     @adds_context('SaveFromDeathContext')
     def handle_save_from_death_intent(self, message):
-        self.speak("The dark side of the Force is a pathway to many abilities some consider to be unnatural.", expect_response=True)
+        if self.wav_mode:
+            play(os.path.join(skill_path,'audio/tragedy of darth plagueis 3.wav'))
+            led.wait(8)
+            self.speak(" ", expect_response=True)
+        else:
+            self.speak("The dark side of the Force is a pathway to many abilities some consider to be unnatural.", expect_response=True)
+
         self.darkside = True
+
 
     @intent_handler(IntentBuilder('WhatHappened').require('happened').require('PlagueisContext').build())
     @adds_context('SaveFromDeathContext')
     def what_happened_intent(self, message):
-        self.speak("He became so powerful... "
-                   "the only thing he was afraid of was losing his power, which eventually, of course, he did.")
-        self.speak("Unfortunately, he taught his apprentice everything he knew, then his apprentice killed him in his sleep.")
-        sleep(0.4)
-        self.speak("It's ironic he could save others from death, but not himself.", expect_response=True)
+        if self.wav_mode:
+            play(os.path.join(skill_path,'audio/tragedy of darth plagueis 4.wav'))
+            led.wait(31)
+            self.speak(" ", expect_response=True)
+        else:
+            self.speak("He became so powerful... "
+                       "the only thing he was afraid of was losing his power, which eventually, of course, he did.")
+            self.speak("Unfortunately, he taught his apprentice everything he knew, then his apprentice killed him in his sleep.")
+            sleep(0.4)
+            self.speak("It's ironic he could save others from death, but not himself.", expect_response=True)
+
 
     @intent_handler(IntentBuilder('LearnThisPower').require('learn')
                     .require('SaveFromDeathContext').require('PlagueisContext').build())
     @removes_context('PlagueisContext')
     @removes_context('SaveFromDeathContext')
     def handle_can_be_learned_intent(self, message):
-        self.speak("Not from a Jedi")
+        if self.wav_mode:
+            play(os.path.join(skill_path,'audio/tragedy of darth plagueis 5.wav'))
+            led.wait(1, led.LED.ON)
+        else:
+            self.speak("Not from a Jedi")
+
 
     @removes_context('PlagueisContext')
     @removes_context('SaveFromDeathContext')
     def stop(self):
         if self.started and not self.darkside:
-                self.speak("The dark side of the Force is a pathway to many abilities some consider to be unnatural.")
+                if self.wav_mode:
+                    play(os.path.join(skill_path,'audio/tragedy of darth plagueis 3.wav'))
+                    led.wait(8)
+
+                else:
+                    self.speak("The dark side of the Force is a pathway to many abilities some consider to be unnatural.")
 
         return True
 
 
-# The "create_skill()" method is used to create an instance of the skill.
-# Note that it's outside the class itself.
 def create_skill():
-    return PlagueisSkillContext()
+    return PlagueisSkillContext(mode='wav')
